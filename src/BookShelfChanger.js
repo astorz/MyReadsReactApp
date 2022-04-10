b079852dfbd99009fb7705a43c33e92efa8e1f62
‘Andreas <andreasstorz@hotmail.com>
2022-04-10 12:31:12 -0400

    BookShelfChanger to regular function

--- src/BookShelfChanger.js
+++ src/BookShelfChanger.js
@@ -1,34 +1,31 @@
 import React from 'react';
 import * as BooksAPI from './BooksAPI';
 
-class BookShelfChanger extends React.Component {
+const BookShelfChanger = props => {
   
-  handleChange = event => {
+  const handleChange = event => {
     const targetShelf = event.target.value;
-    const book = this.props.book;
-    this.props.updateShelf(book, targetShelf);
+    const book = props.book;
+    props.updateShelf(book, targetShelf);
     BooksAPI.update(book, targetShelf);
   }
-    
-  render() {
 
-    const options = [
-      { value: "currentlyReading", text: "Currently Reading"},
-      { value: "wantToRead", text: "Want to Read"},
-      { value: "read", text: "Read"},
-      { value: "none", text: "None" }
-    ];
+  const options = [
+    { value: "currentlyReading", text: "Currently Reading"},
+    { value: "wantToRead", text: "Want to Read"},
+    { value: "read", text: "Read"},
+    { value: "none", text: "None" }
+  ];
 
-    return (
-      <select value={this.props.shelf} onChange={this.handleChange}>
-        {options.map(o => 
-          <option 
-              value={o.value}
-              key={o.value}
-            >{o.text}</option>)};
-      </select>
-    )
-  }
+  return (
+    <select value={props.shelf} onChange={handleChange}>
+      {options.map(o => 
+        <option 
+            value={o.value}
+            key={o.value}
+          >{o.text}</option>)};
+    </select>
+  )
 }
 
 export default BookShelfChanger;
